621d94065812927b53197b3677148e92df2e81da
slazarska <slazarska@protonmail.com>
2023-02-28 15:18:57 +0300

    added properties, filled main page object and main page tests

--- src/test/java/homeless/tests/pages/MainPage.java
+++ src/test/java/homeless/tests/pages/MainPage.java
@@ -1,4 +1,41 @@
 package homeless.tests.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import homeless.tests.helpers.DriverUtils;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MainPage {
+
+    private static final String MAIN_PAGE_URL = "https://homeless.ru/";
+    private static final String TITLE = "Ночлежка — старейшая благотворительная организация, помогающая бездомным людям.";
+
+    SelenideElement logo = $(".logo-main");
+
+    @Step("Open Main Page")
+    public void openMainPage() {
+        open(MAIN_PAGE_URL);
+    }
+
+    @Step("Check Main Page is opened")
+    public void checkMainPageIsOpened() {
+        logo.shouldBe(Condition.visible);
+    }
+
+    @Step("Check Main page title")
+    public void checkTitle() {
+        String expectedTitle = TITLE;
+        String actualTitle = title();
+        assertThat(actualTitle).isEqualTo(expectedTitle);
+    }
+
+    @Step("Check Main page console log have no errors")
+    public void checkConsoleLogHaveNoErrors() {
+        String consoleLogs = DriverUtils.getConsoleLogs();
+        String errorText = "SEVERE";
+        assertThat(consoleLogs).doesNotContain(errorText);
+    }
 }
--- src/test/java/homeless/tests/tests/MainPageTests.java
+++ src/test/java/homeless/tests/tests/MainPageTests.java
@@ -1,4 +1,31 @@
 package homeless.tests.tests;
 
+import homeless.tests.pages.MainPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MainPageTests {
+
+    MainPage mainPage = new MainPage();
+
+    @Test
+    @DisplayName("Open main page")
+    void openMainPageTest() {
+        mainPage.openMainPage();
+        mainPage.checkMainPageIsOpened();
+    }
+
+    @Test
+    @DisplayName("Main page title should have header text")
+    void mainPageTitleTest() {
+        mainPage.openMainPage();
+        mainPage.checkTitle();
+    }
+
+    @Test
+    @DisplayName("Page console log should have no errors")
+    void consoleLogTest() {
+        mainPage.openMainPage();
+        mainPage.checkConsoleLogHaveNoErrors();
+    }
 }
